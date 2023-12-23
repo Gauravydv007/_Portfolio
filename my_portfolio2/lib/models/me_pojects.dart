7b8ae6053e1a9f17e6ecdfbf30d6cc1481751735
Gaurav Yadav <Gaurav88689548@gmail.com>
2023-12-23 22:35:01 +0530

    Add My Projects description

--- my_portfolio2/lib/models/me_pojects.dart
+++ my_portfolio2/lib/models/me_pojects.dart
@@ -11,14 +11,14 @@ List<Project> demo_projects = [
         "On Flutter V2.* web officially supported on a stable branch. Today I share an Admin panel or you can call it dashboard UI build with flutter. Now you can build your app dashboard using flutter. This dashboard contains almost everything that you need like a chart, table, nice small card for showing info.",
   ),
   Project(
-    title: "E-Commerce Complate App - Flutter UI",
+    title: "ERP  App - Flutter UI",
     description:
-        "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+               "Welcome to CampusFlow, the comprehensive ERP (Enterprise Resource Planning) app designed exclusively for college life. Streamline your academic journey and campus experience with this intuitive application that seamlessly integrates various functionalities to enhance communication, collaboration, and organization.",
   ),
   Project(
-    title: "Outlook Email App Redesign - Flutter Fully Responsive Design UI",
+    title: "Hunch App  - Flutter Fully Responsive Design UI",
     description:
-        "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+             "Make decisions confidently with Hunch, your intuitive decision-making app designed to streamline choices and provide personalized insights. Whether you're deciding what to wear, where to eat, or important life choices, Hunch is here to guide you through the decision-making process.",
   ),
   Project(
     title: "Chat/Messaging App Light and Dark Theme - Flutter UI",
@@ -31,8 +31,8 @@ List<Project> demo_projects = [
         "In the first part of our complete e-commerce app, we show you how you can create a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
   ),
   Project(
-    title: "Covid-19 App - Flutter UI",
+    title: "Weather App (For Live Information) - Flutter UI",
     description:
-        "We redesign the outlook app also make it responsive so that you can run it everywhere on your phone, tab, or web. In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+        "Stay ahead of the elements with WeatherWise, the most intuitive and comprehensive weather app designed to provide you with accurate and up-to-the-minute weather information. Whether you're planning your day, a weekend getaway, or a week-long vacation, WeatherWise is your go-to app for all things weather-related.",
   ),
 ];
